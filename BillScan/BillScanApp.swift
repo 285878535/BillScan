005b2a7ee55541a0acef37991ede9054000de3b3
justinxing <285878535@qq.com>
2026-04-28 22:31:02 +0800

    Initial Commit

---
+++ BillScan/BillScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillScanApp.swift
+//  BillScan
+//
+//  Created by Justin Xing on 2026/4/28.
+//
+
+import SwiftUI
+
+@main
+struct BillScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BillScan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BillScan
+//
+//  Created by Justin Xing on 2026/4/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
